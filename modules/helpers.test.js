12014f1e19c1d78163287ff073208700e476d9f4
Damian Poole <damian.poole@gmail.com>
2019-01-04 10:44:24 +0000

    ADD: Initial test class for Helpers

---
+++ modules/helpers.test.js
@@ -0,0 +1,36 @@
+/*globals jest*/
+const Helpers = require('./helpers');
+
+describe("Helper class", () => {
+    describe("isSameObject", () => {
+        it("Should return true exact same object", () => {
+            const object = {
+                property: "test"
+            };
+
+            expect(Helpers.isSameObject(object, object)).toEqual(true);
+        });
+
+        it("Should return true for two objects with the same properties", () => {
+            const object1 = {
+                property: "test"
+            };
+            const object2 = {
+                property: "test"
+            };
+
+            expect(Helpers.isSameObject(object1, object2)).toEqual(true);
+        });
+
+        it("Should return false for two objects with different properties", () => {
+            const object1 = {
+                property: "foo"
+            };
+            const object2 = {
+                property: "bar"
+            };
+
+            expect(Helpers.isSameObject(object1, object2)).toEqual(false);
+        });
+    });
+});
